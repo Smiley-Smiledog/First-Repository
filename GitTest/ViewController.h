0a699e1ab390d8a22aa68376abd884d9ca43715a
Simon Westren <simonwestren@gmail.com>
2014-10-06 18:55:24 +1100

    Initial commit from local git

---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Simon Westren on 6/10/2014.
+//  Copyright (c) 2014 Simon Westren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by Simon Westren on 6/10/2014.
+//  Copyright (c) 2014 Simon Westren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

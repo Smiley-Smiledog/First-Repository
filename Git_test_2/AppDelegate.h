29461265471e87789bdd97a129e5e06760540952
Simon Westren <simonwestren@gmail.com>
2014-10-07 09:01:12 +1100

    Initial commit from local git

---
+++ Git_test_2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git_test_2
+//
+//  Created by Simon Westren on 7/10/2014.
+//  Copyright (c) 2014 Simon Westren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git_test_2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Git_test_2
+//
+//  Created by Simon Westren on 7/10/2014.
+//  Copyright (c) 2014 Simon Westren. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
